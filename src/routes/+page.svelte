cc0a4e8707252b88d5249f90b11d9f3b871cc6ec
ccalobeto <calobeto.leon@gmail.com>
2024-09-16 16:11:48 -0500

    publish style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Protocol } from 'pmtiles';
+	import maplibregl from 'maplibre-gl';
+	import { onMount } from 'svelte';
+	import { base } from '$app/paths';
+
+	const style = `${base}/data/styles/grayscale.json`;
+
+	let protocol = new Protocol();
+	maplibregl.addProtocol('pmtiles', protocol.tile);
+
+	onMount(() => {
+		const initialize = { lng: -77.03778, lat: -12.06649, zoom: 13.5 };
+		const map = new maplibregl.Map({
+			container: 'map',
+			zoom: initialize.zoom,
+			center: [initialize.lng, initialize.lat],
+			style: style
+		});
+	});
+</script>
+
+<div id="map"></div>
+
+<style>
+	#map {
+		position: absolute;
+		top: 0;
+		bottom: 0;
+		width: 100%;
+	}
+</style>

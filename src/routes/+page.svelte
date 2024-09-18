8826ee9873c911a42eb50df621f40823d619b484
ccalobeto <calobeto.leon@gmail.com>
2024-09-18 11:56:41 -0500

    use pmtile for uk

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,14 @@
 	import { onMount } from 'svelte';
 	import { base } from '$app/paths';
 
-	const style = `${base}/data/grayscale.json`;
+	const style = `${base}/data/gb-style.json`;
 
 	let protocol = new Protocol();
 	maplibregl.addProtocol('pmtiles', protocol.tile);
 
 	onMount(() => {
-		const initialize = { lng: -77.03778, lat: -12.06649, zoom: 13.5 };
+		// const initialize = { lng: -77.03778, lat: -12.06649, zoom: 13.5 };// Lima
+		const initialize = { lng: -5.031075, lat: 53.90009, zoom: 5 }; // Dublin
 		const map = new maplibregl.Map({
 			container: 'map',
 			zoom: initialize.zoom,

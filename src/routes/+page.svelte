c4969afba14a44e9d5ed77917c22000542837ee3
ccalobeto <calobeto.leon@gmail.com>
2025-04-05 17:08:46 -0500

    config protomap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,19 +4,19 @@
 	import { onMount } from 'svelte';
 	import { base } from '$app/paths';
 
-	// const style = `${base}/data/gb-style.json`;
+	const style = `${base}/data/peru-style.json`;
+	// const style = ''https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json'';
 
-	// let protocol = new Protocol();
-	// maplibregl.addProtocol('pmtiles', protocol.tile);
+	let protocol = new Protocol();
+	maplibregl.addProtocol('pmtiles', protocol.tile);
 
 	onMount(() => {
 		const initialize = { lng: -77.03778, lat: -12.06649, zoom: 13.5 }; // Lima
-		// const initialize = { lng: -5.031075, lat: 53.90009, zoom: 5 }; // United Kingdom
 		const map = new maplibregl.Map({
 			container: 'map',
 			zoom: initialize.zoom,
 			center: [initialize.lng, initialize.lat],
-			style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json'
+			style: style
 		});
 	});
 </script>

643efd78bec824fc33535144cf852f9d36d02bef
ccalobeto <calobeto.leon@gmail.com>
2025-04-06 12:05:03 -0500

    set carto style

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,17 @@
 <script>
-	import { Protocol } from 'pmtiles';
+	// import { Protocol } from 'pmtiles';
 	import maplibregl from 'maplibre-gl';
 	import { onMount } from 'svelte';
-	import { base } from '$app/paths';
+	// import { base } from '$app/paths';
 
-	const style = `${base}/data/peru-style.json`;
-	// const style = ''https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json'';
+	// const style = `${base}/data/peru-style.json`;
+	const style = 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json';
 
-	let protocol = new Protocol();
-	maplibregl.addProtocol('pmtiles', protocol.tile);
+	// let protocol = new Protocol();
+	// maplibregl.addProtocol('pmtiles', protocol.tile);
 
 	onMount(() => {
-		const initialize = { lng: -77.03778, lat: -12.06649, zoom: 13.5 }; // Lima
+		const initialize = { lng: -77.03778, lat: -12.06649, zoom: 13 }; // Lima
 		const map = new maplibregl.Map({
 			container: 'map',
 			zoom: initialize.zoom,
